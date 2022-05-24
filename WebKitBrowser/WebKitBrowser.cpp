04deb23953d72f17de877be5ab3e47b01e7187de
Mikolaj Staworzynski <mikolaj.staworzynski@redembedded.com>
2022-05-24 17:28:19 +0200

    ONEM-25655: WebkitBrowser plugin compilation for R3

--- WebKitBrowser/WebKitBrowser.cpp
+++ WebKitBrowser/WebKitBrowser.cpp
@@ -445,7 +445,7 @@ namespace WebKitBrowser {
             _children = Core::ProcessInfo::Iterator(_main.Id());
             return ((_startTime == 0) || (_main.IsActive() == true) ? 1 : 0) + _children.Count();
         }
-        const bool IsOperational() const override
+        bool IsOperational() const override
         {
             uint32_t requiredProcesses = 0;
 
