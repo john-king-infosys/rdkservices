532a92a11f4487c6a9bbf837f76da0cf2082e2c2
tomasz-karczewski-red <tomasz.karczewski@redembedded.com>
2022-07-20 08:41:22 +0000

    cherry picks applied

---
+++ helpers/UtilsJsonRpc.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "UtilsLogging.h"
+
+#define LOGINFOMETHOD() { std::string json; parameters.ToString(json); LOGINFO( "params=%s", json.c_str() ); }
+#define LOGTRACEMETHODFIN() { std::string json; response.ToString(json); LOGINFO( "response=%s", json.c_str() ); }
+#define returnResponse(success) \
+    { \
+        response["success"] = success; \
+        LOGTRACEMETHODFIN(); \
+        return (Core::ERROR_NONE); \
+    }
+#define returnIfParamNotFound(param, name) \
+    if (!param.HasLabel(name)) \
+    { \
+        LOGERR("No argument '%s'", name); \
+        returnResponse(false); \
+    }
+#define returnIfStringParamNotFound(param, name) \
+    if (!param.HasLabel(name) || param[name].Content() != Core::JSON::Variant::type::STRING) \
+    {\
+        LOGERR("No argument '%s' or it has incorrect type", name); \
+        returnResponse(false); \
+    }
+#define returnIfBooleanParamNotFound(param, name) \
+    if (!param.HasLabel(name) || param[name].Content() != Core::JSON::Variant::type::BOOLEAN) \
+    { \
+        LOGERR("No argument '%s' or it has incorrect type", name); \
+        returnResponse(false); \
+    }
+#define returnIfNumberParamNotFound(param, name) \
+    if (!param.HasLabel(name) || param[name].Content() != Core::JSON::Variant::type::NUMBER) \
+    { \
+        LOGERR("No argument '%s' or it has incorrect type", name); \
+        returnResponse(false); \
+    }
+#define sendNotify(event,params) { \
+    std::string json; \
+    params.ToString(json); \
+    LOGINFO("Notify %s %s", event, json.c_str()); \
+    Notify(event,params); \
+}
+#define getNumberParameter(paramName, param) { \
+    if (Core::JSON::Variant::type::NUMBER == parameters[paramName].Content()) \
+        param = parameters[paramName].Number(); \
+    else \
+        try { param = std::stoi( parameters[paramName].String()); } \
+        catch (...) { param = 0; } \
+}
+#define getNumberParameterObject(parameters, paramName, param) { \
+    if (Core::JSON::Variant::type::NUMBER == parameters[paramName].Content()) \
+        param = parameters[paramName].Number(); \
+    else \
+        try {param = std::stoi( parameters[paramName].String());} \
+        catch (...) { param = 0; } \
+}
+#define getBoolParameter(paramName, param) { \
+    if (Core::JSON::Variant::type::BOOLEAN == parameters[paramName].Content()) \
+        param = parameters[paramName].Boolean(); \
+    else \
+        param = parameters[paramName].String() == "true" || parameters[paramName].String() == "1"; \
+}
+#define getStringParameter(paramName, param) { \
+    if (Core::JSON::Variant::type::STRING == parameters[paramName].Content()) \
+        param = parameters[paramName].String(); \
+}
