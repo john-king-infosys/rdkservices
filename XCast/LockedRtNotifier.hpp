4c3f039eb1097e7e325a10b19afbfa5741aab5ad
tomasz-karczewski-red <tomasz.karczewski@redembedded.com>
2023-02-20 15:31:06 +0000

    OMWAPPI-1077 locked API for XCast

---
+++ XCast/LockedRtNotifier.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "RtNotifier.h"
+#include <mutex>
+#include "UtilsSynchro.hpp"
+
+class LockedRtNotifier : public RtNotifier {
+    RtNotifier *wrapped;
+    std::recursive_mutex& mtx;
+public:
+    LockedRtNotifier(RtNotifier *wrapped, std::recursive_mutex& mtx) : wrapped(wrapped), mtx(mtx) {
+    }
+    void onRtServiceDisconnected() {
+        std::unique_lock<std::recursive_mutex> lock(mtx);
+        wrapped->onRtServiceDisconnected();
+    };
+    void onXcastApplicationLaunchRequest(string appName, string parameter) {
+        std::unique_lock<std::recursive_mutex> lock(mtx);
+        wrapped->onXcastApplicationLaunchRequest(appName, parameter);
+    };
+    void onXcastApplicationLaunchRequestWithLaunchParam (string appName, string strPayLoad, string strQuery, string strAddDataUrl) {
+        std::unique_lock<std::recursive_mutex> lock(mtx);
+        wrapped->onXcastApplicationLaunchRequestWithLaunchParam(appName, strPayLoad, strQuery, strAddDataUrl);
+    };
+    void onXcastApplicationStopRequest(string appName, string appID) {
+        std::unique_lock<std::recursive_mutex> lock(mtx);
+        wrapped->onXcastApplicationStopRequest(appName, appID);
+    };
+    void onXcastApplicationHideRequest(string appName, string appID) {
+        std::unique_lock<std::recursive_mutex> lock(mtx);
+        wrapped->onXcastApplicationHideRequest(appName, appID);
+    };
+    void onXcastApplicationResumeRequest(string appName, string appID) {
+        std::unique_lock<std::recursive_mutex> lock(mtx);
+        wrapped->onXcastApplicationResumeRequest(appName, appID);
+    };
+    void onXcastApplicationStateRequest(string appName, string appID) {
+        std::unique_lock<std::recursive_mutex> lock(mtx);
+        wrapped->onXcastApplicationStateRequest(appName, appID);
+    };
+};
