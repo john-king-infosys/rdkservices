b5a184f63b978e0fe4e7d48b04b7aae1d1d0329a
tomasz-karczewski-red <tomasz.karczewski@redembedded.com>
2023-06-30 11:53:14 +0200

    ONEM-30855 apply R4.2_DeviceInfo_compilation_error.patch
     Conflicts:
    	DeviceInfo/DeviceInfo.cpp
    	DeviceInfo/DeviceInfo.h
    	DeviceInfo/Implementation/DeviceInfo.h

--- DeviceInfo/DeviceInfo.cpp
+++ DeviceInfo/DeviceInfo.cpp
@@ -62,8 +62,8 @@ namespace Plugin {
         ASSERT(_subSystem != nullptr);
 
         _deviceInfo = service->Root<Exchange::IDeviceInfo>(_connectionId, 2000, _T("DeviceInfoImplementation"));
-        _deviceAudioCapabilities = service->Root<Exchange::IDeviceAudioCapabilities>(_connectionId, 2000, _T("DeviceAudioCapabilities"));
-        _deviceVideoCapabilities = service->Root<Exchange::IDeviceVideoCapabilities>(_connectionId, 2000, _T("DeviceVideoCapabilities"));
+        _deviceAudioCapabilityInterface = service->Root<Exchange::IDeviceAudioCapabilities>(_connectionId, 2000, _T("DeviceAudioCapabilities"));
+        _deviceVideoCapabilityInterface = service->Root<Exchange::IDeviceVideoCapabilities>(_connectionId, 2000, _T("DeviceVideoCapabilities"));
         _firmwareVersion = service->Root<Exchange::IFirmwareVersion>(_connectionId, 2000, _T("FirmwareVersion"));
 
         ASSERT(_deviceInfo != nullptr);
@@ -75,8 +75,8 @@ namespace Plugin {
 
         return ((_subSystem != nullptr)
                    && (_deviceInfo != nullptr)
-                   && (_deviceAudioCapabilities != nullptr)
-                   && (_deviceVideoCapabilities != nullptr)
+                   && (_deviceAudioCapabilityInterface != nullptr)
+                   && (_deviceVideoCapabilityInterface != nullptr)
                    && (_firmwareVersion != nullptr))
             ? EMPTY_STRING
             : _T("Could not retrieve System Information.");
@@ -87,8 +87,8 @@ namespace Plugin {
         ASSERT(_service == service);
 
         _deviceInfo->Release();
-        _deviceAudioCapabilities->Release();
-        _deviceVideoCapabilities->Release();
+        _deviceAudioCapabilityInterface->Release();
+        _deviceVideoCapabilityInterface->Release();
         _firmwareVersion->Release();
 
         if (_subSystem != nullptr) {
@@ -169,7 +169,7 @@ namespace Plugin {
         systemInfo.Freeswap = singleton.GetFreeSwap();
         systemInfo.Devicename = singleton.GetHostName();
         systemInfo.Cpuload = Core::NumberType<uint32_t>(static_cast<uint32_t>(singleton.GetCpuLoad())).Text();
-        systemInfo.Serialnumber = _systemId;
+//        systemInfo.Serialnumber = _systemId;
 
         auto cpuloadavg = singleton.GetCpuLoadAvg();
         if (cpuloadavg != nullptr) {
@@ -210,6 +210,139 @@ namespace Plugin {
     void DeviceInfo::SocketPortInfo(JsonData::DeviceInfo::SocketinfoData& socketPortInfo) const
     {
         socketPortInfo.Runs = Core::ResourceMonitor::Instance().Runs();
+    }
+
+    uint32_t DeviceInfo::AudioOutputs(AudioOutputTypes& audioOutputs) const
+    {
+        Exchange::IDeviceAudioCapabilities::IAudioOutputIterator* audioIt;
+
+        uint32_t status = _deviceAudioCapabilityInterface->AudioOutputs(audioIt);
+        if ((status == Core::ERROR_NONE) && (audioIt != nullptr)) {
+            Exchange::IDeviceAudioCapabilities::AudioOutput audioOutput;
+            Core::JSON::EnumType<JsonData::DeviceInfo::AudioportType> jsonAudioOutput;
+            while (audioIt->Next(audioOutput) == true) {
+                jsonAudioOutput = static_cast<JsonData::DeviceInfo::AudioportType>(audioOutput);
+                audioOutputs.Add(jsonAudioOutput);
+            }
+            audioIt->Release();
+        }
+        return status;
+    }
+
+    uint32_t DeviceInfo::AudioCapabilities(const Exchange::IDeviceAudioCapabilities::AudioOutput audioOutput, AudioCapabilityTypes& audioCapabilityTypes) const
+    {
+        Exchange::IDeviceAudioCapabilities::IAudioCapabilityIterator* audioCapabilityIt = nullptr;
+        Exchange::IDeviceAudioCapabilities::AudioCapability audioCapabilty(
+            Exchange::IDeviceAudioCapabilities::AudioCapability::AUDIOCAPABILITY_NONE);
+
+        uint32_t status = _deviceAudioCapabilityInterface->AudioCapabilities(audioOutput, audioCapabilityIt);
+        if ((status == Core::ERROR_NONE) && (audioCapabilityIt != nullptr)) {
+            Core::JSON::EnumType<JsonData::DeviceInfo::AudiocapabilityType> jsonAudioCapability;
+            while (audioCapabilityIt->Next(audioCapabilty)) {
+                jsonAudioCapability = static_cast<JsonData::DeviceInfo::AudiocapabilityType>(audioCapabilty);
+                audioCapabilityTypes.Add(jsonAudioCapability);
+            }
+            audioCapabilityIt->Release();
+        }
+
+        return (status);
+    }
+
+    uint32_t DeviceInfo::Ms12Capabilities(const Exchange::IDeviceAudioCapabilities::AudioOutput audioOutput, Ms12CapabilityTypes& ms12CapabilityTypes) const
+    {
+        Exchange::IDeviceAudioCapabilities::IMS12CapabilityIterator* ms12CapabilityIt = nullptr;
+        Exchange::IDeviceAudioCapabilities::MS12Capability ms12Capabilty(
+            Exchange::IDeviceAudioCapabilities::MS12Capability::MS12CAPABILITY_NONE);
+
+        uint32_t status = _deviceAudioCapabilityInterface->MS12Capabilities(audioOutput, ms12CapabilityIt);
+        if ((status == Core::ERROR_NONE) && (ms12CapabilityIt != nullptr)) {
+            Core::JSON::EnumType<JsonData::DeviceInfo::Ms12capabilityType> jsonMs12Capability;
+            while (ms12CapabilityIt->Next(ms12Capabilty)) {
+                jsonMs12Capability = static_cast<JsonData::DeviceInfo::Ms12capabilityType>(ms12Capabilty);
+                ms12CapabilityTypes.Add(jsonMs12Capability);
+            }
+            ms12CapabilityIt->Release();
+        }
+
+        return (status);
+    }
+
+    uint32_t DeviceInfo::Ms12Profiles(const Exchange::IDeviceAudioCapabilities::AudioOutput audioOutput, Ms12ProfileTypes& ms12ProfileTypes) const
+    {
+        Exchange::IDeviceAudioCapabilities::IMS12ProfileIterator* ms12ProfileIt = nullptr;
+        Exchange::IDeviceAudioCapabilities::MS12Profile ms12Profile(
+            Exchange::IDeviceAudioCapabilities::MS12Profile::MS12PROFILE_NONE);
+
+        uint32_t status = _deviceAudioCapabilityInterface->MS12AudioProfiles(audioOutput, ms12ProfileIt);
+        if ((status == Core::ERROR_NONE) && (ms12ProfileIt != nullptr)) {
+            Core::JSON::EnumType<JsonData::DeviceInfo::Ms12profileType> jsonMs12Profile;
+            while (ms12ProfileIt->Next(ms12Profile)) {
+                jsonMs12Profile = static_cast<JsonData::DeviceInfo::Ms12profileType>(ms12Profile);
+                ms12ProfileTypes.Add(jsonMs12Profile);
+            }
+            ms12ProfileIt->Release();
+        }
+
+        return (status);
+    }
+
+    uint32_t DeviceInfo::VideoOutputs(VideoOutputTypes& videoOutputs) const
+    {
+        Exchange::IDeviceVideoCapabilities::IVideoOutputIterator* videoIt;
+
+        uint32_t status = _deviceVideoCapabilityInterface->VideoOutputs(videoIt);
+        if ((status == Core::ERROR_NONE) && (videoIt != nullptr)) {
+            Exchange::IDeviceVideoCapabilities::VideoOutput videoOutput;
+            Core::JSON::EnumType<JsonData::DeviceInfo::VideodisplayType> jsonVideoOutput;
+            while (videoIt->Next(videoOutput) == true) {
+                jsonVideoOutput = static_cast<JsonData::DeviceInfo::VideodisplayType>(videoOutput);
+                videoOutputs.Add(jsonVideoOutput);
+            }
+            videoIt->Release();
+        }
+        return status;
+    }
+
+    uint32_t DeviceInfo::DefaultResolution(const Exchange::IDeviceVideoCapabilities::VideoOutput videoOutput, ScreenResolutionType& screenResolutionType) const
+    {
+        Exchange::IDeviceVideoCapabilities::ScreenResolution defaultResolution(
+            Exchange::IDeviceVideoCapabilities::ScreenResolution::ScreenResolution_Unknown);
+        uint32_t status = _deviceVideoCapabilityInterface->DefaultResolution(videoOutput, defaultResolution);
+
+        if (status == Core::ERROR_NONE) {
+            screenResolutionType = static_cast<JsonData::DeviceInfo::Output_resolutionType>(defaultResolution);
+        }
+        return (status);
+    }
+
+    uint32_t DeviceInfo::Resolutions(const Exchange::IDeviceVideoCapabilities::VideoOutput videoOutput, ScreenResolutionTypes& screenResolutionTypes) const
+    {
+        Exchange::IDeviceVideoCapabilities::IScreenResolutionIterator* resolutionIt = nullptr;
+        Exchange::IDeviceVideoCapabilities::ScreenResolution resolution(
+            Exchange::IDeviceVideoCapabilities::ScreenResolution::ScreenResolution_Unknown);
+
+        uint32_t status = _deviceVideoCapabilityInterface->Resolutions(videoOutput, resolutionIt);
+        if ((status == Core::ERROR_NONE) && (resolutionIt != nullptr)) {
+            Core::JSON::EnumType<JsonData::DeviceInfo::Output_resolutionType> jsonResolution;
+            while (resolutionIt->Next(resolution)) {
+                jsonResolution = static_cast<JsonData::DeviceInfo::Output_resolutionType>(resolution);
+                screenResolutionTypes.Add(jsonResolution);
+            }
+            resolutionIt->Release();
+        }
+        return (status);
+    }
+
+    uint32_t DeviceInfo::Hdcp(const Exchange::IDeviceVideoCapabilities::VideoOutput videoOutput, CopyProtectionType& copyProtectionType) const
+    {
+        Exchange::IDeviceVideoCapabilities::CopyProtection hdcp(
+            Exchange::IDeviceVideoCapabilities::CopyProtection::HDCP_UNAVAILABLE);
+
+        uint32_t status = _deviceVideoCapabilityInterface->Hdcp(videoOutput, hdcp);
+        if (status == Core::ERROR_NONE) {
+            copyProtectionType = static_cast<JsonData::DeviceInfo::CopyprotectionType>(hdcp);
+        }
+        return (status);
     }
 
 } // namespace Plugin
