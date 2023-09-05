866485e15c2e5bd7d7d688c24c5a300c7c5059af
Jacek Skiba <jacek.skiba@consult.red>
2023-09-05 10:09:26 +0200

    [ONEM-31311] Set UserAgent based on browserversion property

--- WebKitBrowser/WebKitImplementation.cpp
+++ WebKitBrowser/WebKitImplementation.cpp
@@ -565,6 +565,7 @@ static GSourceFuncs _handlerIntervention =
         public:
             Config()
                 : Core::JSON::Container()
+                , BrowserVersion()
                 , WebkitDebug()
                 , GstDebug()
                 , GstNoColor()
@@ -635,6 +636,7 @@ static GSourceFuncs _handlerIntervention =
                 , LoggingTarget()
                 , WebAudioEnabled(false)
             {
+                Add(_T("browserversion"), &BrowserVersion);
                 Add(_T("webkitdebug"), &WebkitDebug);
                 Add(_T("gstdebug"), &GstDebug);
                 Add(_T("gstnocolor"), &GstNoColor);
@@ -712,6 +714,7 @@ static GSourceFuncs _handlerIntervention =
             }
 
         public:
+            Core::JSON::String BrowserVersion;
             Core::JSON::String WebkitDebug;
             Core::JSON::String GstDebug;
             Core::JSON::Boolean GstNoColor;
@@ -1070,7 +1073,6 @@ static GSourceFuncs _handlerIntervention =
                 [](gpointer customdata) {
                     delete static_cast<SetUserAgentData*>(customdata);
                 });
-
             return Core::ERROR_NONE;
         }
 
@@ -3136,7 +3138,7 @@ static GSourceFuncs _handlerIntervention =
             if (_config.UserAgent.IsSet() == true && _config.UserAgent.Value().empty() == false) {
                 webkit_settings_set_user_agent(preferences, _config.UserAgent.Value().c_str());
             } else {
-                webkit_settings_set_user_agent_with_application_details(preferences, "WPE", "1.0");
+                webkit_settings_set_user_agent_with_application_details(preferences, "WPE", _config.BrowserVersion.Value().c_str());
                 _config.UserAgent = webkit_settings_get_user_agent(preferences);
             }
 
